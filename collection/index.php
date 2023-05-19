6c3dbc68b33f566951c71fe5439398315d383343
latgit <rezky.wahyudi1231@gmail.com>
2023-05-19 19:50:54 +0800

    fix books item layout desktop & mobile

--- collection/index.php
+++ collection/index.php
@@ -22,9 +22,7 @@ if (!isset($_SESSION["signin"]) && !isset($_SESSION["signinUser"])) {
     <link rel="stylesheet" href="../css/collection.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-
     <link rel="icon" type="image/svg+xml" href="../assets/favicon/ad-light.svg" media="(prefers-color-scheme: dark)">
     <link rel="icon" type="image/svg+xml" href="../assets/favicon/ad-dark.svg" media="(prefers-color-scheme: light)">
 </head>
@@ -34,12 +32,11 @@ if (!isset($_SESSION["signin"]) && !isset($_SESSION["signinUser"])) {
         <a href="../"><img src="../assets/icon/ad-logo.svg" alt="Aksara Diskret Logo"></a>
         <nav>
             <ul id="nav-list">
-                <li>
-                    <?php if (!isset($_SESSION["signinUser"])) : ?>
+                <?php if (!isset($_SESSION["signinUser"])) : ?>
+                    <li>
                         <a href="../admin">Admin</a>
-                    <?php endif; ?>
-                </li>
-
+                    </li>
+                <?php endif; ?>
                 <li><a href="../faq">FAQ</a></li>
                 <li><a href="../about">About</a></li>
                 <li id="close-icon" onclick="closeMenu()">
@@ -60,9 +57,8 @@ if (!isset($_SESSION["signin"]) && !isset($_SESSION["signinUser"])) {
                             <?php if (isset($_SESSION["userName"])) : ?>
                                 <?= $_SESSION["userName"] ?>
                             <?php else : ?>
-                                Admin Aksara-Diskret
+                                Admin
                             <?php endif; ?>
-
                         </span>
                         <a href="../settings"><img src="../assets/icon/remixicon-settings-3-line.svg" alt="Settings Icon"></a>
                     </div>
@@ -73,66 +69,82 @@ if (!isset($_SESSION["signin"]) && !isset($_SESSION["signinUser"])) {
             <h1>Books Collection</h1>
             <div class="books-content">
                 <a href="../assets/book/AD-Secret-Book.pdf" class="rounded-box books-item" download>
-                    <img loading="lazy" src="../assets/image/deepmind-esyG2Jt_uIc-unsplash.jpg" alt="Book Cover">
-                    <div class="books-info">
-                        <h2>Learning Algorithm</h2>
-                        <p>Karen Liu</p>
+                    <div class="book">
+                        <img loading="lazy" src="../assets/image/deepmind-esyG2Jt_uIc-unsplash.jpg" alt="Book Cover">
+                        <div class="books-info">
+                            <h2>Learning Algorithm</h2>
+                            <p>Karen Liu</p>
+                        </div>
                     </div>
                     <div class="isbn"><b>ISBN</b><br>9787825426666</div>
                 </a>
                 <a href="../assets/book/AD-Secret-Book.pdf" class="rounded-box books-item" download>
-                    <img loading="lazy" src="../assets/image/deepmind-X5CSjHTjlgw-unsplash.jpg" alt="Book Cover">
-                    <div class="books-info">
-                        <h2>Neural Network</h2>
-                        <p>Andrew Kim</p>
+                    <div class="book">
+                        <img loading="lazy" src="../assets/image/deepmind-X5CSjHTjlgw-unsplash.jpg" alt="Book Cover">
+                        <div class="books-info">
+                            <h2>Neural Network</h2>
+                            <p>Andrew Kim</p>
+                        </div>
                     </div>
                     <div class="isbn"><b>ISBN</b><br>9786011789073</div>
                 </a>
                 <a href="../assets/book/AD-Secret-Book.pdf" class="rounded-box books-item" download>
-                    <img loading="lazy" src="../assets/image/deepmind-mWztzk66I7Q-unsplash.jpg" alt="Book Cover">
-                    <div class="books-info">
-                        <h2>Singularity Paradox</h2>
-                        <p>Olivia Chen</p>
+                    <div class="book">
+                        <img loading="lazy" src="../assets/image/deepmind-mWztzk66I7Q-unsplash.jpg" alt="Book Cover">
+                        <div class="books-info">
+                            <h2>Singularity Paradox</h2>
+                            <p>Olivia Chen</p>
+                        </div>
                     </div>
                     <div class="isbn"><b>ISBN</b><br>9780258222782</div>
                 </a>
                 <a href="../assets/book/AD-Secret-Book.pdf" class="rounded-box books-item" download>
-                    <img loading="lazy" src="../assets/image/deepmind-3VSgApkySLA-unsplash.jpg" alt="Book Cover">
-                    <div class="books-info">
-                        <h2>AI Rebellion</h2>
-                        <p>Ethan Zhang</p>
+                    <div class="book">
+                        <img loading="lazy" src="../assets/image/deepmind-3VSgApkySLA-unsplash.jpg" alt="Book Cover">
+                        <div class="books-info">
+                            <h2>AI Rebellion</h2>
+                            <p>Ethan Zhang</p>
+                        </div>
                     </div>
                     <div class="isbn"><b>ISBN</b><br>9782335341133</div>
                 </a>
                 <a href="../assets/book/AD-Secret-Book.pdf" class="rounded-box books-item" download>
-                    <img loading="lazy" src="../assets/image/wilhelm-gunkel-aEECAd2HuUE-unsplash.jpg" alt="Book Cover">
-                    <div class="books-info">
-                        <h2>The Glass House</h2>
-                        <p>Jane Sanchez</p>
+                    <div class="book">
+                        <img loading="lazy" src="../assets/image/wilhelm-gunkel-aEECAd2HuUE-unsplash.jpg" alt="Book Cover">
+                        <div class="books-info">
+                            <h2>The Glass House</h2>
+                            <p>Jane Sanchez</p>
+                        </div>
                     </div>
                     <div class="isbn"><b>ISBN</b><br>9782463696112</div>
                 </a>
                 <a href="../assets/book/AD-Secret-Book.pdf" class="rounded-box books-item" download>
-                    <img loading="lazy" src="../assets/image/mk-s-_j0Wjh0Ya8I-unsplash.jpg" alt="Book Cover">
-                    <div class="books-info">
-                        <h2>Designing Realities</h2>
-                        <p>Kevin Chen</p>
+                    <div class="book">
+                        <img loading="lazy" src="../assets/image/mk-s-_j0Wjh0Ya8I-unsplash.jpg" alt="Book Cover">
+                        <div class="books-info">
+                            <h2>Designing Realities</h2>
+                            <p>Kevin Chen</p>
+                        </div>
                     </div>
                     <div class="isbn"><b>ISBN</b><br>9785240080548</div>
                 </a>
                 <a href="../assets/book/AD-Secret-Book.pdf" class="rounded-box books-item" download>
-                    <img loading="lazy" src="../assets/image/ivan-aleksic-FoYLV60_eHY-unsplash.jpg" alt="Book Cover">
-                    <div class="books-info">
-                        <h2>Blueprint for Chaos</h2>
-                        <p>Matthew Davis</p>
+                    <div class="book">
+                        <img loading="lazy" src="../assets/image/ivan-aleksic-FoYLV60_eHY-unsplash.jpg" alt="Book Cover">
+                        <div class="books-info">
+                            <h2>Blueprint for Chaos</h2>
+                            <p>Matthew Davis</p>
+                        </div>
                     </div>
                     <div class="isbn"><b>ISBN</b><br>9785862813142</div>
                 </a>
                 <a href="../assets/book/AD-Secret-Book.pdf" class="rounded-box books-item" download>
-                    <img loading="lazy" src="../assets/image/erol-ahmed-5nFU8l0RDiY-unsplash.jpg" alt="Book Cover">
-                    <div class="books-info">
-                        <h2>The Hidden City </h2>
-                        <p>Carlos Rodriguez</p>
+                    <div class="book">
+                        <img loading="lazy" src="../assets/image/erol-ahmed-5nFU8l0RDiY-unsplash.jpg" alt="Book Cover">
+                        <div class="books-info">
+                            <h2>The Hidden City </h2>
+                            <p>Carlos Rodriguez</p>
+                        </div>
                     </div>
                     <div class="isbn"><b>ISBN</b><br>9782463696112</div>
                 </a>
